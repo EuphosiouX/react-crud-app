27c4160536605edf32b079af8edc3a2d71093d64
Michael Christopher <michaelchristopher1009@gmail.com>
2025-05-23 21:03:19 +1000

    create jest config file

--- jest-junit.config.js
+++ jest-junit.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   outputDirectory: 'reports/test',
   outputName: 'junit.xml',
+      testMatch: [
+        '<rootDir>/src/**/*.test.js',
+        '<rootDir>/src/**/*.test.jsx',
+    ],
 };
